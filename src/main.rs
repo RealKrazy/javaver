f012a8a7d79c24fb2bb8c86c409e9135def75745
Real Krazy <krazyproandlovefe2@gmail.com>
2024-08-06 20:50:50 +0200

    Proper version selection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-use std::{env, fs, path::PathBuf};
+extern crate winapi;
+
+use std::collections::HashMap;
+use std::io;
+use std::{env, ffi::OsString, fs, iter, path::PathBuf};
+use std::os::windows::ffi::OsStrExt;
 
 use clap::{Parser, Subcommand};
 use javaver::config::{self, JavaverConfig, SDKConfig};
+use winapi::shared::minwindef::LPARAM;
+use winapi::um::winuser;
+use winreg::RegKey;
 
 const CONFIG_PATH: &str = "javaver-config.json";
 
@@ -35,7 +43,7 @@ enum Commands {
         name: String,
 
     },
-    /// Remove an already-added SDK
+    /// Remove an already-added SDK from the list
     Rm {
 
         name: String,
@@ -43,6 +51,63 @@ enum Commands {
     },
     /// List all added SDKs
     List,
+}
+
+fn get_exe_dir() -> io::Result<PathBuf> {
+    let mut path = env::current_exe()?;
+    path.pop();
+
+    Ok(path)
+}
+
+fn encode_win_string(str: &str) -> Vec<u16> {
+    OsString::from(str)
+    .encode_wide()
+    .chain(iter::once(0))
+    .collect()
+}
+
+fn read_system_vars() -> HashMap<String, String> {
+    let hkcu = RegKey::predef(winreg::enums::HKEY_LOCAL_MACHINE);
+    let env = hkcu.open_subkey_with_flags("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", winreg::enums::KEY_READ)
+        .expect("Failed to open registry keys");
+
+    let mut env_vars = HashMap::new();
+    for (name, value) in env.enum_values().map(|x| x.unwrap()) {
+        env_vars.insert(name, value.to_string());
+    }
+
+    env_vars
+}
+
+fn read_system_path_var() -> Vec<String> {
+    let vars = read_system_vars();
+    let path = vars.iter().find(|el| el.0.to_lowercase() == "path").unwrap();
+
+    path.1.split(';').map(|entry| entry.to_string()).collect()
+}
+
+fn set_system_path_var(value: &str) -> io::Result<()> {
+    let hkcu = RegKey::predef(winreg::enums::HKEY_LOCAL_MACHINE);
+    let env = hkcu.open_subkey_with_flags("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", winreg::enums::KEY_WRITE)?;
+
+    env.set_value("Path", &value)
+}
+
+fn notify_env_change() {
+    let event = encode_win_string("Environment");
+
+    unsafe {
+        winuser::SendMessageTimeoutW(
+            winuser::HWND_BROADCAST,
+            winuser::WM_SETTINGCHANGE,
+            0,
+            event.as_ptr() as LPARAM,
+            winuser::SMTO_ABORTIFHUNG,
+            5000,
+            std::ptr::null_mut(),
+        );
+    }
 }
 
 fn validate_java_path(path: &PathBuf) -> bool {
@@ -62,7 +127,7 @@ fn validate_java_path(path: &PathBuf) -> bool {
 
 fn main() {
     let mut config;
-    let config_path = PathBuf::from(CONFIG_PATH);
+    let config_path = get_exe_dir().unwrap().join(PathBuf::from(CONFIG_PATH));
 
     if config_path.exists() {
         match config::read_config(&config_path) {
@@ -148,15 +213,37 @@ fn main() {
             }
 
             let sdk = config.sdk.iter().find(|sdk| &sdk.name == name).unwrap();
-            let path = &sdk.path;
+            let path = sdk.path.to_str().unwrap();
+            let path_bin = &sdk.path.join("bin");
+            let path_bin = path_bin.to_str().unwrap();
 
-            println!("{}", env::var("Path").unwrap());
+            let mut path_vars = read_system_path_var();
+            if let Some(pos) = path_vars.iter().position(|el| el.to_lowercase() == path_bin.to_lowercase()) {
+                path_vars.remove(pos);
+            }
 
-            env::set_var("JAVA_HOME", path.to_str().unwrap());
+            path_vars.insert(0, path_bin.to_owned());
+
+            if let Err(err) = set_system_path_var(path_vars.join(";").as_str()) {
+                println!("Error when modifying system-wide 'Path' environment variable: {}\nYou might not be running as an administrator.", err);
+                return;
+            }
+            
+            notify_env_change();
+
+            env::set_var("JAVA_HOME", path);
             println!("Successfully selected '{}' as current Java SDK", name);
         },
         Some(Commands::Rm { name }) => {
+            if !config.contains_name(name.as_str()) {
+                println!("There is no SDK under the name '{}'.", name);
+                return;
+            }
 
+            let pos = config.sdk.iter().position(|el| el.name == name.to_owned()).unwrap();
+            config.sdk.remove(pos);
+
+            println!("Successfully removed '{}' from the SDK list.", name);
         }
         Some(Commands::List) => {
             if config.sdk.is_empty() {
